ec0ef697789c104e177bd6fa266b73e3933e999c
William Hincapié Medina <daemonsoft@gmail.com>
2025-07-04 14:50:47 -0500

    Add H2 Database configuration and initialization

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE prices
+(
+    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
+    brand_id      BIGINT         NOT NULL,
+    valid_from    TIMESTAMP      NOT NULL,
+    valid_until   TIMESTAMP      NOT NULL,
+    rate          BIGINT         NOT NULL,
+    product_id    BIGINT         NOT NULL,
+    priority      INT            NOT NULL,
+    final_price   DECIMAL(10, 2) NOT NULL,
+    currency_code VARCHAR(3)     NOT NULL
+);
+
+CREATE INDEX idx_prices_product_brand_dates
+    ON prices (product_id, brand_id, valid_from, valid_until, priority DESC);
